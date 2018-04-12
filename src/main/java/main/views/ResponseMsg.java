6cc6ea5b11af2f8a3743090b907cc55675818653
Mahmud <qudusov98@mail.ru>
2018-04-13 00:41:57 +0300

    added error code in ResponseMsg view

--- src/main/java/main/views/ResponseMsg.java
+++ src/main/java/main/views/ResponseMsg.java
@@ -5,20 +5,26 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 @SuppressWarnings("unused")
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum ResponseMsg {
-    @SuppressWarnings("EnumeratedConstantNamingConvention") OK("OK"),
-    CREATED("User Created"),
-    FORBIDDEN("Invalid auth data"),
-    BAD_REQUEST("Not all Json fields are filled"),
-    NOT_LOGGED_IN("You are not autorized"),
-    CONFLICT("Login or Email are already exist"),
-    INVALID_LOGIN("User with such auth data is not registered"),
-    INCORRECT_PASSWORD("Incorrect password"),
-    INTERNAL_SERVER_ERROR("Server error. Sorry:(");
+    NOT_LOGGED_IN(0, "You are not autorized"),
+    @SuppressWarnings("EnumeratedConstantNamingConvention") OK(1, "OK"),
+    CREATED(2, "User Created"),
+    FORBIDDEN(3, "Invalid auth data"),
+    BAD_REQUEST(4, "Not all Json fields are filled"),
+    CONFLICT(5, "Login or Email are already exist"),
+    INVALID_LOGIN(6, "User with such auth data is not registered"),
+    INCORRECT_PASSWORD(7, "Incorrect password"),
+    INTERNAL_SERVER_ERROR(8, "Server error. Sorry:(");
 
+    private final Integer status;
     private final String msg;
 
-    ResponseMsg(String msg) {
+    ResponseMsg(Integer status, String msg) {
+        this.status = status;
         this.msg = msg;
+    }
+
+    public Integer getStatus() {
+        return status;
     }
 
     public String getMsg() {
